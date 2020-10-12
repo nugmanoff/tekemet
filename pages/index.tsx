947de510a36f6069221dcb8007e3fcee5fd1d7c5
yerlan <temirerlan99@gmail.com>
2020-10-12 20:14:46 +0300

    feat(index): add player & background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,32 @@
 export default function Home() {
-  return <div>Tekemet!</div>;
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+        <img
+          src="/images/background.jpg"
+          style={{
+            objectFit: 'cover',
+            height: '100%',
+            width: '100%',
+            zIndex: 0,
+          }}
+        />
+      </div>
+      <audio
+        controls
+        src="http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p"
+        autoPlay
+      >
+        {' '}
+      </audio>
+    </div>
+  );
 }
